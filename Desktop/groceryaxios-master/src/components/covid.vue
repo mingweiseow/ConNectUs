ae32fc0982c50db376c30c2800ad4e343362c69d
lohennqi <ennqiii@gmail.com>
2020-03-31 04:30:45 +0800

    covid-chart

---
+++ Desktop/groceryaxios-master/src/components/covid.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="main">
+        <div id = "chart">
+        <linechartCovid></linechartCovid>
+        </div>
+
+    </div>
+</template>
+
+<script>
+import linechartCovid from '../linechartCovid.js'
+export default {
+    components:{
+        linechartCovid
+    }
+    
+}
+</script>>
+<style scoped>
+#main{
+    content:"";
+    display:table;
+    clear:both;
+}
+#chart{
+  height:1000px;
+  width:1000px;
+  border-style: solid;
+  border-color:rgb(155, 84, 84);
+  border-width: 1px;
+  padding:30px;
+  float:left;
+  position:center;
+}
+</style>>
---
+++ Desktop/groceryaxios-master/src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    //Key in your database details
+    apiKey: "AIzaSyD30e1CgsAZTSKIOI_yovNlAifq-M4VC_w",
+    authDomain: "axios-chart.firebaseapp.com",
+    databaseURL: "https://axios-chart.firebaseio.com",
+    projectId: "axios-chart"
+
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.firestore();
+  export default database;
+
+  
